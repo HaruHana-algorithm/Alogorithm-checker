361a888948f215f17bb6539abfd28db55b080790
seonghoo1217 <seonghoo1217@naver.com>
2023-04-24 20:40:13 +0900

    #3 feat : 회원 목록 반환 API

--- checker/src/main/java/haruhana/checker/controller/UserController.java
+++ checker/src/main/java/haruhana/checker/controller/UserController.java
@@ -1,10 +1,14 @@
 package haruhana.checker.controller;
 
+import haruhana.checker.entity.Member;
 import haruhana.checker.service.GithubService;
+import haruhana.checker.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,11 +17,15 @@ public class UserController {
 
 	private final GithubService githubService;
 
+	private final MemberService memberService;
+
 	@GetMapping("/contributors")
 	public void getRepoContributors(){
 		githubService.getRepoContributors();
 	}
 
 	@GetMapping("/memberList")
-
+	public List<Member> getInfoMemberList(){
+		return memberService.getInfoMemberList();
+	}
 }
